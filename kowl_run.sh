4ff215a659b5ecd8c230539a2cec59a46baa42d3
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-02-10 10:49:29 -0300

    Update kowl_run.sh

--- kowl_run.sh
+++ kowl_run.sh
@@ -1,1 +1,1 @@
-echo "kafka:" >>kowl_config.yaml; echo "  brokers:" >>kowl_config.yaml; echo "    - 0.0.0.0:9092" >>kowl_config.yaml
+echo "kafka:" >>kowl_config.yaml; echo "  brokers:" >>kowl_config.yaml; echo "    - kafka1:19092" >>kowl_config.yaml
